54bad2c82b6e44b4576d275b6f1492c5fdfb45f0
Min Thet Naing <bobo.b90@gmail.com>
2022-11-27 00:33:15 +0630

    Add formatting original_url

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,11 +4,50 @@ class Link < ApplicationRecord
   validates :original_url, presence: true
   validates :shortened_url, uniqueness: true
 
-  before_create :generate_shortened_url
+  before_create :format_original_url, :generate_shortened_url
 
   private
 
   def generate_shortened_url
     self.shortened_url = SecureRandom.base58(6)
+  end
+
+  def format_original_url
+    self.original_url = format_url(original_url)
+
+    raise ActiveRecord::RecordInvalid unless valid_url?(original_url)
+  end
+
+  def format_url(url)
+    original_scheme = get_original_scheme(url)
+    splited_url = url.split("//")
+    url = splited_url.length > 1 ? splited_url[1] : splited_url[0]
+    url.prepend("://").prepend(original_scheme || "http")
+  end
+
+  def get_original_scheme(url)
+    %w[https http].find { |valid_scheme| valid_scheme if url.start_with?(valid_scheme) }
+  end
+
+  def valid_url?(url)
+    return false unless url
+
+    uri = URI.parse(url)
+    return false unless uri.host
+
+    valid_uri_scheme?(uri) && valid_uri_suffix?(uri)
+  end
+
+  def valid_uri_scheme?(uri)
+    return true unless uri&.scheme
+
+    valid_scheme = %w[http https]
+    valid_scheme.include?(uri.scheme)
+  end
+
+  def valid_uri_suffix?(uri)
+    return false unless uri&.host
+
+    uri.host.split(".")[1].present?
   end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -21,12 +21,129 @@ RSpec.describe Link do
     describe "before_create" do
       let(:link) { Fabricate.create(:link) }
 
-      it "generates shortened url" do
-        expect(link.shortened_url).not_to be_nil
+      describe ".generate_shortened_url" do
+        it "generates shortened url" do
+          expect(link.shortened_url).not_to be_nil
+        end
+
+        it "does not update shortened_url if it is already present" do
+          expect { link.update!(original_url: "https://www.google.com") }.not_to change(link, :shortened_url)
+        end
       end
 
-      it "does not update shortened_url if it is already present" do
-        expect { link.update!(original_url: "https://www.google.com") }.not_to change(link, :shortened_url)
+      describe ".format_original_url" do
+        let(:expected_url) { Faker::Internet.url }
+
+        shared_examples_for "formatted original url" do
+          let(:link) { Fabricate.create(:link, original_url: original_url) }
+          it "returns the formatted original url" do
+            expect(link.original_url).to eq expected_url
+          end
+        end
+
+        context "when the original_url is valid" do
+          context "with full path without www" do
+            it_behaves_like "formatted original url" do
+              let(:original_url) { "http://fake.com" }
+              let(:expected_url) { "http://fake.com" }
+            end
+          end
+
+          context "with full path including www" do
+            it_behaves_like "formatted original url" do
+              let(:original_url) { "http://www.fake.com" }
+              let(:expected_url) { "http://www.fake.com" }
+            end
+          end
+
+          context "with full path including www and using https" do
+            it_behaves_like "formatted original url" do
+              let(:original_url) { "https://www.fake.com" }
+              let(:expected_url) { "https://www.fake.com" }
+            end
+          end
+
+          context "without proper http scheme" do
+            context "without www" do
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "fake.com" }
+                let(:expected_url) { "http://fake.com" }
+              end
+
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "fake.abcdfakeagain" }
+                let(:expected_url) { "http://fake.abcdfakeagain" }
+              end
+
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "fake.com/test_api" }
+                let(:expected_url) { "http://fake.com/test_api" }
+              end
+
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "//fake.com" }
+                let(:expected_url) { "http://fake.com" }
+              end
+
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "://fake.com" }
+                let(:expected_url) { "http://fake.com" }
+              end
+
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "htp://fake.com" }
+                let(:expected_url) { "http://fake.com" }
+              end
+
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "http//fake.com" }
+                let(:expected_url) { "http://fake.com" }
+              end
+
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "https//fake.com" }
+                let(:expected_url) { "https://fake.com" }
+              end
+            end
+
+            context "with www" do
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "www.fake.com" }
+                let(:expected_url) { "http://www.fake.com" }
+              end
+
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "www.fake" }
+                let(:expected_url) { "http://www.fake" }
+              end
+
+              it_behaves_like "formatted original url" do
+                let(:original_url) { "www.fake.com/test_api" }
+                let(:expected_url) { "http://www.fake.com/test_api" }
+              end
+            end
+          end
+        end
+
+        context "when the original_url is invalid" do
+          shared_examples_for "raise record invalid error" do
+            it "returns the formatted original url" do
+              expect { Fabricate.create(:link, original_url: original_url) }.to raise_error(ActiveRecord::RecordInvalid)
+            end
+          end
+
+          context "without proper domain" do
+            it_behaves_like "raise record invalid error" do
+              let(:original_url) { "http://fake" }
+            end
+          end
+
+          context "with wrong http scheme" do
+            it_behaves_like "raise record invalid error" do
+              let(:original_url) { "/fake.com" }
+            end
+          end
+        end
       end
     end
   end
